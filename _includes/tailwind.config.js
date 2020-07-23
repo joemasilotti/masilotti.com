ba29b694ce32ae343cdebaec47643b6aa6533108
Joe Masilotti <joe@masilotti.com>
2020-07-23 10:55:11 -0400

    First pass at design and import three posts

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,8 +1,42 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  important: "html", // Override typography plugin with more specific selectors.
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.green
+      }
+    },
+    rotate: {
+      "-10": "-10deg",
+      "0": "0",
+      "10": "10deg"
+    },
+    typography: theme => ({
+      default: {
+        css: {
+          maxWidth: null,
+          code: {
+            backgroundColor: theme("colors.gray.200"),
+            paddingTop: theme("spacing.1"),
+            paddingRight: theme("spacing.2"),
+            paddingBottom: theme("spacing.1"),
+            paddingLeft: theme("spacing.2"),
+            borderRadius: theme("borderRadius.default")
+          },
+          "code::before": null,
+          "code::after": null,
+        }
+      }
+    })
   },
-  variants: {},
-  plugins: [],
-}
+  variants: {
+    textColor: ["active"],
+    rotate: ["group-hover"]
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+};
