f42799c02b4e6dc69450d79b3c2b76930a824f20
Joe Masilotti <joe@masilotti.com>
2022-12-17 09:31:26 -0800

    SEO tags and metadata

--- src/_components/head.rb
+++ src/_components/head.rb
@@ -1,7 +1,40 @@
 class Head < SiteComponent
-  attr_reader :title
+  attr_reader :page, :metadata
 
-  def initialize(title:)
-    @title = title
+  def initialize(page:, metadata:)
+    @page, @metadata = page, metadata
+  end
+
+  def title
+    [page.data.title, metadata.title].compact.join(" | ")
+  end
+
+  def description
+    page.data.description.presence || metadata.description
+  end
+
+  def author
+    metadata.author
+  end
+
+  def url
+    page.absolute_url
+  end
+
+  def image
+    absolute_url(page.data.image || metadata.image)
+  end
+
+  def site_name
+    metadata.title
+  end
+
+  def twitter
+    "@#{metadata.twitter}"
+  end
+
+  def twitter_card
+    # TODO: Always large?
+    "summary_large_image"
   end
 end
