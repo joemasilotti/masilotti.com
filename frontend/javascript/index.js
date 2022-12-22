873fb797dba9d859671f57f012f7352b80476db0
Joe Masilotti <joe@masilotti.com>
2022-12-21 20:54:43 -0800

    Syntax highlighting CSS

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,4 +1,5 @@
 import "index.css"
+import "syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
