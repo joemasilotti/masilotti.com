a3668944b8c4d164d1f061a827fa877e21ca11b6
Joe Masilotti <joe@masilotti.com>
2023-01-25 12:30:56 -0800

    Remove prices from non-meeting services

--- src/_components/service.rb
+++ src/_components/service.rb
@@ -1,7 +1,7 @@
 class Service < SiteComponent
   attr_reader :title, :price, :best_if, :you_get, :description, :href
 
-  def initialize(title:, price:, best_if:, you_get:, description:, href:)
+  def initialize(title:, price: nil, best_if:, you_get:, description:, href:)
     @title, @price, @best_if, @you_get, @description, @href =
       title, price, best_if, you_get, description, href
   end
