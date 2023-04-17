b359c7bd4b30bf733e2af5da486fca1b1d7cc58e
Joe Masilotti <joe@masilotti.com>
2023-04-17 16:01:06 -0700

    Add a badge to the workshop link on homepage

--- src/_components/core_offering.rb
+++ src/_components/core_offering.rb
@@ -1,8 +1,8 @@
 class CoreOffering < SiteComponent
-  attr_reader :title, :icon, :href, :description, :cta
+  attr_reader :title, :icon, :href, :description, :cta, :badge
 
-  def initialize(title:, icon:, href:, description:, cta:)
-    @title, @icon, @href, @description, @cta =
-      title, icon, href, description, cta
+  def initialize(title:, icon:, href:, description:, cta:, badge: nil)
+    @title, @icon, @href, @description, @cta, @badge =
+      title, icon, href, description, cta, badge
   end
 end
