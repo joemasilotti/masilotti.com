a7cb010eafdae49c5b287e5c1b13476cee7a55a4
Joe Masilotti <joe@masilotti.com>
2023-04-21 15:50:30 -0700

    Clean up core offerings on small desktop screens

--- src/_components/ui/container.rb
+++ src/_components/ui/container.rb
@@ -1,7 +1,8 @@
 class UI::Container < SiteComponent
-  attr_reader :class_name
+  attr_reader :class_name, :inner_class_name
 
-  def initialize(class_name: nil)
+  def initialize(class_name: nil, inner_class_name: nil)
     @class_name = class_name
+    @inner_class_name = inner_class_name
   end
 end
