0d13ca4a3e453f1ec5baf7f2a16dbe7278f76a79
Joe Masilotti <joe@masilotti.com>
2023-04-24 14:06:09 -0700

    /newsletter

--- src/_components/footer.rb
+++ src/_components/footer.rb
@@ -1,7 +1,7 @@
 class Footer < SiteComponent
-  attr_reader :links, :newsletter
+  attr_reader :links, :newsletter, :hide_newsletter
 
-  def initialize(links:, newsletter:)
-    @links, @newsletter = links, newsletter
+  def initialize(links:, newsletter:, hide_newsletter: false)
+    @links, @newsletter, @hide_newsletter = links, newsletter, hide_newsletter
   end
 end
