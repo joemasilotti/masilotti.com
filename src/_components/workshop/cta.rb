fdf4a5fdae2a978b852f74369da72adb46f9cd25
Joe Masilotti <joe@masilotti.com>
2023-10-25 10:09:00 -0700

    Make the workshop CTA easier to change

--- src/_components/workshop/cta.rb
+++ src/_components/workshop/cta.rb
@@ -1,9 +1,22 @@
 module Workshop
   class CTA < SiteComponent
+    DATE = Time.use_zone("Pacific Time (US & Canada)") do
+      Time.zone.local(2023, 11, 17, 10)
+    end
+
     attr_reader :newsletter
 
     def initialize(newsletter:, event_id:, hide_count: false)
       @newsletter, @event_id, @hide_count = newsletter, event_id, hide_count
+    end
+
+    def past_date?
+      Date.today > DATE
+    end
+
+    def date
+      end_time = (DATE + 2.hours).strftime("%l%P")
+      DATE.strftime("%A, %B %e at %l%P-#{end_time} PT on Zoom | Cost: #{price} per person")
     end
 
     def event_id
@@ -29,6 +42,10 @@ module Workshop
 
     def tickets
       ENV.fetch("WORKSHOP_TICKETS_REMAINING").to_i
+    end
+
+    def price
+      "$99"
     end
   end
 end
